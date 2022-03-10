40908ba7e6eee4f7694850789c1c3eee718df5ff
Minh Thong <lmthong97@gmail.com>
2022-03-10 09:49:48 +0700

    Add loading - Add skeleton category - Fix img resize

---
+++ src/features/Product/components/CategorySkeletonList.jsx
@@ -0,0 +1,33 @@
+import { Box, Grid, Skeleton } from '@mui/material';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+CategorySkeletonList.propTypes = {
+    length: PropTypes.number,
+
+};
+CategorySkeletonList.defaultProps = {
+    length: 6   
+}
+
+function CategorySkeletonList({ length }) {
+    
+    const listSkeleton = Array.from(new Array(length))
+    return (
+        <Box>
+            <Grid container>
+                {listSkeleton.map((x,index) =>(
+
+                    <Grid item key={index} xs={12}>
+                        <Box padding={1}>
+                            <Skeleton />
+                            
+                        </Box>
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
+    );
+}
+
+export default CategorySkeletonList;
